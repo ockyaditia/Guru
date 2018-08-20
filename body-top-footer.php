f490c222623390b8d547edad82a01aeab7dcee42
Ocky Aditia Saputra <ocky-aditia.saputra@idemia.com>
2018-08-20 08:16:00 +0700

    ADDED: Footer menu

--- body-top-footer.php
+++ body-top-footer.php
@@ -6,8 +6,65 @@
                         <div class="footer-logo">
                             <a href="index.php"><img src="img/core-img/guru.png" width="96" height="15" alt=""></a>
                         </div>
+						<br>
+						<br>
+						<div>
+							<font size="3">
+								<table style="color:white">
+									<th style="width:15%;">Tentang Kami</th>
+									<th style="width:15%;">Produk Kami</th>
+									<th style="width:15%;">Kurikulum</th>
+									<th style="width:15%;">Mata Pelajaran</th>
+									<th style="width:15%;">Lainnya</th>
+									<tr>
+										<td><p><a href="about-us.php">Tentang Kami</a></p></td>
+										<td><p><a href="online-learn.php">Belajar Online</a></p></td>
+										<td><p><a href="online-learn-category.php?category=Sekolah Dasar (SD)">Sekolah Dasar (SD)</a></p></td>
+										<td><p><a href="online-learn-theory.php?code=AGAMA_ISLAM_SD&subject=Agama Islam&class=Sekolah Dasar (SD)">Agama Islam Sekolah Dasar (SD)</a></p></td>
+										<td><p><a href="blog.php">Blog</a></p></td>
+									</tr>
+									<tr>
+										<td><p><a href="help.php">Bantuan</a></p></td>
+										<td><p><a href="online-tutoring.php">Les Online</a></p></td>
+										<td><p><a href="online-learn-category.php?category=Sekolah Menengah Pertama (SMP)">Sekolah Menengah Pertama (SMP)</a></p></td>
+										<td><p></p></td>
+										<td><p><a href="#">Kebijakan Privasi</a></p></td>
+									</tr>
+									<tr>
+										<td><p><a href="#">Kontak Kami</a></p></td>
+										<td><p><a href="digital-bootcamp.php">Les Online</a></p></td>
+										<td><p><a href="online-learn-category.php?category=Sekolah Menengah Atas (SMA)">Sekolah Menengah Atas (SMA)</a></p></td>
+										<td><p></p></td>
+										<td><p><a href="#">Syarat dan Ketentuan</a></p></td>
+									</tr>
+									<tr>
+										<td><p></p></td>
+										<td><p><a href="online-tryout.php">Tryout Online</a></p></td>
+										<td><p><a href="online-learn-category.php?category=Sekolah Menengah Kejuruan (SMK)">Sekolah Menengah Kejuruan (SMK)</a></p></td>
+										<td><p></p></td>
+										<td><p></p></td>
+									</tr>
+									<tr>
+										<td><p></p></td>
+										<td><p><a href="private-tutoring.php">Les Private</a></p></td>
+										<td><p></p></td>
+										<td><p></p></td>
+										<td><p></p></td>
+									</tr>
+									<tr>
+										<td><p></p></td>
+										<td><p><a href="virtual-class.php">Kelas Virtual</a></p></td>
+										<td><p></p></td>
+										<td><p></p></td>
+										<td><p></p></td>
+									</tr>
+								</table>
+							</font>
+						</div>
+						<br>
+						<br>
                         <!-- Copywrite -->
-                        <p><a href="#"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        <p><a href="index.php"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 							Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Guru</a>
 							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                     </div>
